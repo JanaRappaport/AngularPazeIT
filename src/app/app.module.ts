c4177e396751624e511f4e477db0c6ed12f6f008
hodaya.farkash@gmail.com <hodaya.farkash@gmail.com>
2022-08-30 19:06:03 +0300

    commit19:06

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { UserComponent } from './components/user/user.component';
-import { LogInComponent } from './components/log-in/log-in.component'
+import { LogInComponent } from './components/log-in/log-in.component';
+import { EntryComponent } from './components/entry/entry.component'
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    LogInComponent
+    LogInComponent,
+    EntryComponent
   ],
   imports: [
     BrowserModule,
