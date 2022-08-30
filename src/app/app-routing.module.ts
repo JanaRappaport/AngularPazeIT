d32497f814d3427737119194d16fbdda9d5d771f
hodaya.farkash@gmail.com <hodaya.farkash@gmail.com>
2022-08-30 19:05:38 +0300

    commit19:05

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LogInComponent } from './components/log-in/log-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {path:"logIn", component:LogInComponent} 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
-import { UserComponent } from './components/user/user.component'
+import { UserComponent } from './components/user/user.component';
+import { LogInComponent } from './components/log-in/log-in.component'
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
