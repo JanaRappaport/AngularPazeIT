415d9997ed6a5a79bff569ba2e21cfd8549fdf33
shira farkash <shirafarkash8@gmail.com>
2022-08-31 19:40:46 +0300

    entery page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EntryComponent } from './components/entry/entry.component';
 import { LogInComponent } from './components/log-in/log-in.component';
 
 const routes: Routes = [
- {path:"logIn", component:LogInComponent} 
+ {path:'logIn' ,component:LogInComponent} ,
+ {path:'' , component:EntryComponent}
 ];
 
 @NgModule({
